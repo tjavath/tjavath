1bcaf2d41177614a25a8ec0bb528283f11e27a1e
tjavath <120459634+tjavath@users.noreply.github.com>
2022-12-13 17:07:42 +0530

    mobileno.py

---
+++ mobile number.py
@@ -0,0 +1,10 @@
+# Python program to check if
+# given mobile number is valid
+def valphone(s): 
+    if len(s)==(12):
+        return s(0)== ("(+(1-2){3})-[4-6]{3}[0-9]{4}") 
+    else :
+        print ("Invalid Number")
+phoneInput="+12124567990"
+print(valphone(phoneInput))
+print("End of an application")
